bc6343ff0ca2091890d7c639102bb11f3abee60b
Pablo Marcos Sánchez <pablomarcossanche06.pm@gmail.com>
2021-04-11 18:31:53 +0200

    Initial commit

---
+++ app/src/main/java/com/example/proyecto_final_pms/SecondActivity.kt
@@ -0,0 +1,11 @@
+package com.example.proyecto_final_pms
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_2)
+    }
+}
